5c5c2355afe92844b9fce221414556a48181a2a2
xuantruong11022005-del <xuantruong11022005@gmail.com>
2026-03-22 05:24:48 +0700

    detail

---
+++ js/js/detail.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+<meta charset="UTF-8">
+<title>Chi tiết sản phẩm</title>
+</head>
+
+<body>
+
+<div id="detail"></div>
+
+<script src="js/data.js"></script>
+<script src="js/product-detail.js"></script>
+
+</body>
+</html>
