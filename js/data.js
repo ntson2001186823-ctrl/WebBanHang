31f6ec9ac8a418766ec5563644a8a76a02f5bb51
xuantruong11022005-del <xuantruong11022005@gmail.com>
2026-03-27 23:13:11 +0700

    update mới và hoàn chỉnh

--- js/data.js
+++ js/data.js
@@ -2,6 +2,7 @@ const products = [
     {
         id: 1,
         name: "Laptop Gaming",
+        category: "laptop",
         price: 20000000,
         img: "https://github.com/ntson2001186823-ctrl/WebBanHang/blob/main/images/banner2.png?raw=true",
         desc: "Laptop gaming cấu hình mạnh, chơi game mượt."
@@ -9,6 +10,7 @@ const products = [
     {
         id: 2,
         name: "iPhone 17",
+        category: "phone",
         price: 25000000,
         img: "https://github.com/ntson2001186823-ctrl/WebBanHang/blob/main/images/banner3.png?raw=true",
         desc: "Điện thoại cao cấp mới nhất của Apple."
@@ -16,6 +18,7 @@ const products = [
     {
         id: 3,
         name: "Tai nghe Sony",
+        category: "Tai Nghe Sony",
         price: 2000000,
         img: "https://github.com/ntson2001186823-ctrl/WebBanHang/blob/main/images/headphone.png?raw=true",
         desc: "Tai nghe chống ồn chất lượng cao."
@@ -23,8 +26,10 @@ const products = [
     {
         id: 4,
         name: "Chuột Gaming",
+        category: "accessory",
         price: 500000,
         img: "https://github.com/ntson2001186823-ctrl/WebBanHang/blob/main/images/mouse.png?raw=true",
         desc: "Chuột gaming RGB siêu nhạy."
     }
 ];
+
